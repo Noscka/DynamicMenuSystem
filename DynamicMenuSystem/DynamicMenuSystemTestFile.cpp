f8a62b1208442ec26cfd16beb4e72e6532271776
Ruski1 <adam882200@gmail.com>
2022-03-01 22:00:50 +0000

    Added example explanation

--- DynamicMenuSystem/DynamicMenuSystemTestFile.cpp
+++ DynamicMenuSystem/DynamicMenuSystemTestFile.cpp
@@ -14,8 +14,13 @@ void blank()
 //MenuEntry MenuEntries,
 int main()
 {
-	DynamicMenuSystem::AddMenuEntries(4, MenuEntry("Entry 1", blank), MenuEntry("Entry 2 Eletric boogalu", blank), MenuEntry("Settings?", blank), MenuEntry("Nothing", blank));
-	DynamicMenuSystem::CreateMenu("Test Project Menu", true);
+	DynamicMenuSystem::AddMenuEntries(4, MenuEntry("Entry 1", blank),					// First you have to add Some entries
+										 MenuEntry("Entry 2 Eletric boogalu", blank),	// using the "MenuEntry" object
+										 MenuEntry("Settings?", blank),					// with both a entry name and entry function
+										 MenuEntry("Nothing", blank));					// which will be ran when entry is selected
+
+	DynamicMenuSystem::CreateMenu("Test Project Menu", true);							// Then run the "CreateMenu" function to use the entries
+																						// and create a sorta of nice looking menu
 
 	return 0;
 }
